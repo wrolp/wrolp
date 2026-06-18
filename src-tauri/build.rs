ad6a363710f03db19ba635c0d77b635132dac2df
hgvant <hgtk11@163.com>
2026-06-19 02:11:59 +0800

    style: format build.rs and update gitignore

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,16 +1,16 @@
 use tauri::generate_handler;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(generate_handler![
-            ssh_terminal::commands::list_connections,
-            ssh_terminal::commands::save_connection,
-            ssh_terminal::commands::delete_connection,
-            ssh_terminal::commands::connect,
-            ssh_terminal::commands::disconnect,
-            ssh_terminal::commands::send_input,
-            ssh_terminal::commands::resize_terminal,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(generate_handler![
+      ssh_terminal::commands::list_connections,
+      ssh_terminal::commands::save_connection,
+      ssh_terminal::commands::delete_connection,
+      ssh_terminal::commands::connect,
+      ssh_terminal::commands::disconnect,
+      ssh_terminal::commands::send_input,
+      ssh_terminal::commands::resize_terminal,
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
